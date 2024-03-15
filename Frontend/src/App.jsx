d7e49383cebfbddb1f502fe730f5f8b69e1742f1
Aaditya Saraswat <saraswataaditya60@gmail.com>
2024-03-15 13:44:33 +0530

    Create Home and Navbar

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignUp from './Components/SignUp'
+import Login from './Components/Login'
+
+function App() {
+  return (
+    <div>
+      <SignUp/>
+      <Login/>
+    </div>
+  )
+}
+
+export default App
